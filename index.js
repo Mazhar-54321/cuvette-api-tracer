af2b23121ce10060b525a94af9b87953d492c8fa
Mazhar-54321 <syedmazharali742@gmail.com>
2025-09-20 12:16:18 +0530

    Added first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+function logger(){
+    return (req,res,next)=>{
+        console.log(req);
+        next();
+    }
+}
+app.listen(process.env.PORT,()=>{
+
+})
