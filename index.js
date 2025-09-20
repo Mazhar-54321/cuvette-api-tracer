2e88f7825e99e3f4f2c1e1447724206a25aa2f96
Mazhar-54321 <syedmazharali742@gmail.com>
2025-09-20 16:41:15 +0530

    changes to version

--- index.js
+++ index.js
@@ -2,55 +2,36 @@ import mongoose from "mongoose";
 import database from "./database.js";
 import express from 'express'
 database();
-// Request log schema
-const requestSchema = new mongoose.Schema({
-  method: String,
-  url: String,
-  body: mongoose.Schema.Types.Mixed,
-  query: mongoose.Schema.Types.Mixed,
-  params: mongoose.Schema.Types.Mixed,
-  headers: mongoose.Schema.Types.Mixed,
-  timestamp: { type: Date, default: Date.now }
-});
-
-let RequestLog;
-function initModel() {
-  if (!RequestLog) RequestLog = mongoose.models.RequestLog || mongoose.model("RequestLog", requestSchema);
-}
-
-/**
- * Middleware factory
- * @param {Object} options - { mongoUri: 'your mongodb uri' }
- */
-export function logger(options = {}) {
-  const { mongoUri } = options;
-//   if (!mongoUri) throw new Error("cuvette-api-tracer: mongoUri is required");
-
-//   mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => console.log("cuvette-api-tracer: MongoDB connected"))
-//     .catch(err => console.error("cuvette-api-tracer: MongoDB connection error", err));
-
-  initModel();
-
-  return async (req, res, next) => {
-    try {
-      const log = new RequestLog({
-        method: req.method,
-        url: req.url,
-        body: req.body,
-        query: req.query,
-        params: req.params,
-        headers: req.headers
-      });
-    //   console.log(req,res);
-      await log.save();
-    } catch (err) {
-      console.error("cuvette-api-tracer: failed to save log", err);
+export default function logger(){
+    return async(req,res,next)=>{
+        const apiKey ="qwerty-imadganc-mkuwrtegvsfbas";
+        const start = Date.now();
+        const logBuffer = [];
+        const consoleObj = {
+            log:console.log,
+            error:console.error,
+            warn:console.warn,
+            info:console.info
+        }
+        ["log","error","warn","info"].forEach((method)=>{
+            console[method]=(...args)=>{
+                logBuffer.push({
+                    timestamp: new Date(),
+                    type:method,
+                    message:args.map((a)=>typeof a ==="object"?JSON.stringify(a):a).join(" ")
+                })
+                consoleObj[method](...args)
+            }
+        })
+        res.on("finish",async()=>{
+            const duration = Date.now()-start;
+            Object.keys(consoleObj).forEach((method)=>{
+                console[method]=consoleObj[method];
+            })
+            console.log(res.statusCode,duration,logBuffer)
+        })
+       next();
     }
-
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next();
-  };
 }
 const app = express();
 app.use(express.json())
