7fd06dd7df95f7c926cf7b1ba17628bd41119559
Mazhar-54321 <syedmazharali742@gmail.com>
2025-09-28 12:53:27 +0530

    lib changes

--- index.js
+++ index.js
@@ -1,39 +1,56 @@
-import mongoose from "mongoose";
-import database from "./database.js";
-import express from 'express'
-database();
-export default function logger(){
-    return async(req,res,next)=>{
-        const apiKey ="qwerty-imadganc-mkuwrtegvsfbas";
-        const start = Date.now();
-        const logBuffer = [];
-        const consoleObj = {
-            log:console.log,
-            error:console.error,
-            warn:console.warn,
-            info:console.info
-        }
-        ["log","error","warn","info"].forEach((method)=>{
-            console[method]=(...args)=>{
-                logBuffer.push({
-                    timestamp: new Date(),
-                    type:method,
-                    message:args.map((a)=>typeof a ==="object"?JSON.stringify(a):a).join(" ")
-                })
-                consoleObj[method](...args)
-            }
-        })
-        res.on("finish",async()=>{
-            const duration = Date.now()-start;
-            Object.keys(consoleObj).forEach((method)=>{
-                console[method]=consoleObj[method];
-            })
-            console.log(res.statusCode,duration,logBuffer)
-        })
-       next();
-    }
+import axios from "axios";
+import crypto from "crypto";
+export default function logger() {
+  return async (req, res, next) => {
+    const start = Date.now();
+    const timestamp = new Date().toISOString();
+    const logBuffer = [];
+    const consoleObj = {
+      log: console.log,
+      error: console.error,
+      warn: console.warn,
+      info: console.info,
+    };
+    ["log", "error", "warn", "info"].forEach((method) => {
+      console[method] = (...args) => {
+        logBuffer.push({
+          timestamp: new Date(),
+          type: method,
+          message: args
+            .map((a) => (typeof a === "object" ? JSON.stringify(a) : a))
+            .join(" "),
+        });
+        consoleObj[method](...args);
+      };
+    });
+    res.on("finish", async () => {
+      const duration = Date.now() - start;
+      Object.keys(consoleObj).forEach((method) => {
+        console[method] = consoleObj[method];
+      });
+      const obj = {
+        timestamp: timestamp,
+        apiName: req.originalUrl,
+        method: req.method,
+        traceId: crypto.randomBytes(8).toString("hex"),
+        statusCode: res.statusCode,
+        responseTimeMs: duration,
+        logs: logBuffer,
+      };
+      try {
+        await axios.post(
+          "https://cuvette-api-dashboard.onrender.com/log",
+          obj,
+          {
+            headers: {
+              "x-api-key": req.headers["x-api-key"],
+            },
+          }
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    });
+    next();
+  };
 }
-const app = express();
-app.use(express.json())
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Central logger running on port ${PORT}`));
